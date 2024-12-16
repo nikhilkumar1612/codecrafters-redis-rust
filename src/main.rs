8037e142dea71102572bc01de8dbb3e961265a8f
nikhil kumar <nikhilkumarpps@gmail.com>
2024-12-16 13:05:31 +0530

    fix: port

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::{
 fn main() {
     println!("Logs from your program will appear here!");
     
-    let listener = TcpListener::bind("127.0.0.1:6380").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     
     for stream in listener.incoming() {
         match stream {
