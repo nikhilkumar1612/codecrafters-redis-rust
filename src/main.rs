b3dd891e6b87472f68750bb0800d3b469bbbae6b
nikhil kumar <nikhilkumarpps@gmail.com>
2024-12-16 13:02:44 +0530

    ft: heartbeat message ping/pong impl

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::TcpListener
+};
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
     
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6380").unwrap();
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                let mut buf = [0; 512];
+                _stream.read(&mut buf).unwrap();
+
+                _stream.write(b"+PONG\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
