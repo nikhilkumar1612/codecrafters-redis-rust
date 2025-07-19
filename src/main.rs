88255753260e67bcc73db422f60c73a64b72a8d1
nikhil kumar <nikhilkumarpps@gmail.com>
2025-07-19 11:56:13 +0530

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,34 @@
 #![allow(unused_imports)]
-use std::{
-    io::{Read, Write},
-    net::TcpListener
-};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use std::result::Result::Ok;
+use tokio::net::TcpListener;
 
-fn main() {
-    println!("Logs from your program will appear here!");
-    
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => {
-                println!("accepted new connection");
-                let mut buf = [0; 512];
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-                loop {
-                    let read_count = _stream.read(&mut buf).unwrap();
-                    println!("{}", read_count);
-                    if read_count == 0 {
-                        break;
+    loop {
+        let accept_result = listener.accept().await;
+        match accept_result {
+            Ok((mut stream, addr)) => {
+                // Handle the stream here
+                println!("Accepted connection from: {}", addr);
+
+                tokio::spawn(async move {
+                    let mut buf = [0; 512];
+                    loop {
+                        let read_count = stream.read(&mut buf).await.unwrap();
+                        if read_count == 0 {
+                            break;
+                        }
+        
+                        stream.write(b"+PONG\r\n").await.unwrap();
                     }
-                    _stream.write(b"+PONG\r\n").unwrap();
-                }
+                });
             }
             Err(e) => {
-                println!("error: {}", e);
+                eprintln!("Failed to accept connection: {}", e);
             }
         }
     }
-
-    println!("ending mainnnnn");
 }
